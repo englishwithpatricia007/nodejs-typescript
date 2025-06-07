64aaf1580678c21cbfdfd8ad5e53675c907dc045
Patricia <englishwithpatricia007@gmail.com>
2025-06-07 11:43:42 -0300

    roteamento do projeto

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,6 +2,7 @@ import fastify from 'fastify'
 
 const app = fastify()
 
+app.get('/', () =>  { return "Vá para /hello"})
 app.get('/hello', () => { return "hello, Patrícia!"})
 
 app.listen({port:3333}).then(()=>{console.log("HTTP server running!")})
